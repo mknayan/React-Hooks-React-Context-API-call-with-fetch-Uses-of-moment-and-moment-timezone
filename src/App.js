da5758a03c25f8c22e63c10e247b6bef67fe85eb
Md. Khairullah Nayan <khairullah1172001@gmail.com>
2020-12-11 18:04:54 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row wrapper">
+        structure
+      </div>
+    </div>
+  );
+}
+
+export default App;
